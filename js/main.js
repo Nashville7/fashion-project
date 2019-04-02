3017f453a7cff99aea8a086732a5f94ca9fc2166
Nashville7 <nashvillejames7@gmail.com>
2019-04-02 23:29:11 +0200

    responsiveness

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
-
-// var removeCartItemButtons = document.getElementsByClassName('remove-from-cart')
-// console.log(removeCartItemButtons )
-
-// for (var i = 0; i < removeCartItemButtons.length;) {
-//     var button = removeCartItemButtons[i]
-//     button.addEventListener('click', function() {
-//         console.log('clicked')
-//     })
-// }
-
-
-
-
+function toggleNav(){
+    document.getElementById("navbar").classList.toggle('active');
+}
 
 
 
